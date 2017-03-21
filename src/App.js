401d2d6582528881a54261b804632f3fcffc65d1
Jimmy Lv <jimmy.jinglv@gmail.com>
2017-03-21 19:48:27 +0800

    [#2] - Changed css-modules styles name

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ class App extends Component {
 
   render() {
     return (
-      <View style={styles.app}>
-        <View style={styles.appHeader}>
-          <Text style={styles.appBanner}>Welcome to Rax</Text>
+      <View style={styles['app']}>
+        <View style={styles['header']}>
+          <Text style={styles['banner']}>Welcome to Rax</Text>
         </View>
-        <Text style={styles.appIntro}>
+        <Text style={styles['intro']}>
           To get started, edit src/App.js and save to reload.
         </Text>
       </View>
